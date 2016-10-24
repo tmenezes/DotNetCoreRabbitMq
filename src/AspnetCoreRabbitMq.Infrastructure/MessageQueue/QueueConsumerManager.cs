335e9d97f6b04dedfa1d6d620c64eb500234852f
tmenezes <thiagomenezes2k7@gmail.com>
2016-10-24 01:32:54 -0200

    start consumer development

---
+++ src/AspnetCoreRabbitMq.Infrastructure/MessageQueue/QueueConsumerManager.cs
@@ -0,0 +1,25 @@
+﻿namespace DotNetCoreRabbitMq.Infrastructure.MessageQueue
+{
+    public class QueueConsumerManager
+    {
+        private readonly IConnectionManager _connectionManager;
+
+        public QueueConsumerManager(IConnectionManager connectionManager)
+        {
+            _connectionManager = connectionManager;
+        }
+
+        public void CreateConsumer<T>(string queueName) where T : IMessageQueueService
+        {
+        }
+    }
+
+    public interface IMessageQueueService<in T> : IMessageQueueService where T : class
+    {
+        void ProcessMessage(T message);
+    }
+
+    public interface IMessageQueueService
+    {
+    }
+}
